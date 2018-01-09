586eb7415f8621557f12614eecb38d8b8ce5bbcd
David <david@retailcommon.com>
2018-01-09 12:54:05 -0500

    created project

---
+++ revue-lib/src/test/kotlin/com/github/dmchoull/revue/RevueTest.kt
@@ -0,0 +1,11 @@
+package com.github.dmchoull.revue
+
+import org.amshove.kluent.shouldEqual
+import org.junit.jupiter.api.Test
+
+internal class RevueTest {
+    @Test
+    fun example() {
+        (1 + 1) shouldEqual 2
+    }
+}

a16eb9be61be7306ce5c262d213a7904418410e3
David <david@retailcommon.com>
2018-01-17 10:46:47 -0500

    updated sample app

--- sample/src/main/java/com/github/dmchoull/revuesample/MainActivity.java
+++ sample/src/main/java/com/github/dmchoull/revuesample/MainActivity.java
@@ -2,11 +2,47 @@ package com.github.dmchoull.revuesample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.github.dmchoull.revue.Revue;
+import com.github.dmchoull.revue.builder.RevueDialogBuilder;
+import com.github.dmchoull.revue.builder.SimpleDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
+    // usually would be injected with configuration already done
+    private final Revue revue = new Revue();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radioDialogType = findViewById(R.id.radioDialogType);
+        radioDialogType.setOnCheckedChangeListener((group, checkedId) -> setDialogBuilder(checkedId));
+        setDialogBuilder(radioDialogType.getCheckedRadioButtonId());
+    }
+
+    private void setDialogBuilder(int checkedId) {
+        RevueDialogBuilder builder;
+
+        if (checkedId == R.id.radioSimple) {
+            builder = new SimpleDialogBuilder()
+                    .title("Enjoy using this app?")
+                    .message("Please give us a rating");
+
+            revue.setDialogBuilder(builder);
+        } else {
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onShowNowClick(View view) {
+        revue.showNow(this);
+    }
+
+    public void onShowMaybeClick(View view) {
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
     }
 }
